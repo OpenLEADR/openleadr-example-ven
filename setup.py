0d4eecc6a9be59c7a8bcfdddd521ebbe832eabbb
Atilla <abdullah.atilla@outlook.com>
2020-12-02 10:59:34 +0300

    Add 'tzlocal' as an install requirement
    Add a new requirement  to prevent "TypeError: Only timezones from the pytz library are supported" when it tries to poll.

--- setup.py
+++ setup.py
@@ -7,4 +7,4 @@ with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as file:
 setup(name='openleadr-demo-client',
       version=version,
       description='Example implementation of an OpenADR client using openLEADR',
-      install_requires=[f'openleadr=={version}', 'pyyaml'])
+      install_requires=[f'openleadr=={version}', 'pyyaml', 'tzlocal'])
