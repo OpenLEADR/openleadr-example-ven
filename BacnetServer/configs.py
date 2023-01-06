b611b46f0f5bad0b2adb8395b88a04cefd5df5aa
Ben Bartling <ben.bartling@gmail.com>
2023-01-05 20:07:40 -0600

    Final Tested Server App in Office Lab

--- BacnetServer/configs.py
+++ BacnetServer/configs.py
@@ -3,16 +3,16 @@
 
 # open ADR server params
 VEN_NAME='ven123'
-VTN_URL='http://192.168.0.100:8080/OpenADR2/Simple/2.0b'
+VTN_URL='http://localhost:8080/OpenADR2/Simple/2.0b'
 
 # value from VTN to execute load shed
 LOAD_SHED_GO_VAL = 1
 NORMAL_OPERATIONS = 0
 
 # building main meter MODBUS registries
-MODBUS_METER_ADDRESS = '192.168.0.111'
+MODBUS_METER_ADDRESS = '10.200.200.196'
 MODBUS_METER_PORT = 502
-MODBUS_INPUT_REG = 17
+MODBUS_INPUT_REG = 6004
     
 
     
--- BacnetServer/server_app.py
+++ BacnetServer/server_app.py
@@ -15,8 +15,8 @@ from BAC0.core.devices.local.models import (
 
 import argparse
 import logging
-from configs import MODBUS_METER_ADDRESS, MODBUS_METER_PORT
-from configs import VTN_URL, VEN_NAME, LOAD_SHED_GO_VAL ,NORMAL_OPERATIONS
+from configs import MODBUS_METER_ADDRESS, MODBUS_METER_PORT, MODBUS_INPUT_REG
+from configs import VTN_URL, VEN_NAME, NORMAL_OPERATIONS
 
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
@@ -37,32 +37,7 @@ def make_bacnet_app():
     def check_event_status():
     
         adr_sig_object = bacnet.this_application.get_object_name('ADR-Event-Level')
-        adr_sig_object.presentValue = Real(ven_client.bacnet_payload_value)            
-        #print(f'BACNET API SIGNAL IS: {adr_sig_object.presentValue}')
-                
-        if ven_client.bacnet_payload_value != NORMAL_OPERATIONS:
-            ven_client.bacnet_sig_change_success = True
-            print(f'bacnet_sig_change_success = True')
-        elif (
-            ven_client.bacnet_sig_change_success == True
-            and ven_client.bacnet_sig_revert_success == True
-        ):
-            print("event_checkr RESET ALL VEN PARAMS")
-            ven_client.adr_event_go == False
-            ven_client.adr_event_stop == False
-            ven_client.bacnet_sig_change_success == False
-            ven_client.bacnet_sig_revert_success == False
-            ven_client.adr_payload_value = NORMAL_OPERATIONS
-            
-        elif (
-            ven_client.bacnet_payload_value == NORMAL_OPERATIONS
-            and ven_client.bacnet_sig_change_success == True
-        ):
-            ven_client.bacnet_sig_revert_success = True
-            print('bacnet_sig_revert_success = True')
-                 
-        else:
-            pass
+        adr_sig_object.presentValue = Real(ven_client.bacnet_api_payload_value)            
 
     # create discoverable BACnet object
     _new_objects = analog_value(
@@ -93,8 +68,9 @@ def make_flask_app():
                 "building_meter": ven_client.building_meter,
                 "adr_duration": ven_client.adr_duration,
                 "adr_event_ends": ven_client.adr_event_ends,
+                "adr_event_is_active": ven_client.adr_event_is_active,
                 "event_payload_value": ven_client.event_payload_value,
-                "bacnet_payload_value": ven_client.bacnet_payload_value
+                "bacnet_api_payload_value": ven_client.bacnet_api_payload_value
             }
         return render_template("index.html", event_info=info)
 
@@ -113,12 +89,9 @@ class MyVen():
         self.adr_duration = "Not Set"
         self.adr_event_ends = "Not Set"
         self.event_payload_value = "Not Set"
-        self.bacnet_payload_value = NORMAL_OPERATIONS
-        self.adr_event_go = False
-        self.adr_event_stop = False
-        self.bacnet_sig_change_success = False
-        self.bacnet_sig_revert_success = False
-        self.last_scan = None
+        self.bacnet_api_payload_value = NORMAL_OPERATIONS
+        self.adr_event_is_active = False
+        self.last_scan = datetime.now(timezone.utc)
 
     # this is hit when an ADR event comes in
     def process_adr_event(self, event):
@@ -137,41 +110,64 @@ class MyVen():
             print("adr_event_ends: ", self.adr_event_ends)
 
     async def collect_report_value(self):
-        return self.building_meter
+        await asyncio.gather(
+            asyncio.to_thread(modbus_meter_reader),
+            asyncio.sleep(1))
 
 
-    async def modbus_meter_reader(self):
+    def modbus_meter_reader(self):
 
         try:
-            
             client = ModbusTcpClient(MODBUS_METER_ADDRESS,
                                      port=MODBUS_METER_PORT)
-            result = client.read_input_registers(500,2,units=1)
+            
+            result = client.read_input_registers(MODBUS_INPUT_REG,
+                                                 2,
+                                                 units=1)
             #print(result.registers)
             decoder = BinaryPayloadDecoder.fromRegisters(result.registers, 
                                                          byteorder=Endian.Big)
-            data = decoder.decode_32bit_float()
-            print("MODBUS electric meter read: ", data)
-            self.building_meter = round(data,3)
+            building_meter = round(decoder.decode_32bit_float(),3)
+            print("MODBUS electric meter read: ", building_meter)
+            self.building_meter = building_meter
             client.close()
+            return building_meter
 
         except:
             print("ERROR ON MODBUS METER READ")
             client.close()
             self.building_meter = 1.23
 
-        await asyncio.sleep(30)
-
-    async def event_checkr(self):  # datetime, timezone, timedelta
-
-        while not self.bacnet_sig_revert_success:
-            await self.start_event()
-            await self.stop_event()
-            await self.event_status()
-
-        print("event_checkr while loop FINISHED!")
+    async def event_do(self,delay,item):
+        await asyncio.sleep(delay)
         
+        if item == "go":
+            print("EVENT GO!")
+            self.adr_event_is_active = True
+            self.bacnet_api_payload_value = self.event_payload_value
+            
+        elif item == "stop":
+            print("EVENT STOP!")
+            self.adr_event_is_active = False
+            self.bacnet_api_payload_value = NORMAL_OPERATIONS
+            self.adr_start = "Event has expired"
+            self.adr_duration = "Event has expired"
+            self.adr_event_ends = "Event has expired"
+            self.event_payload_value = "Event has expired"
 
+    async def event_checkr(self):
+        now_utc = datetime.now(timezone.utc)
+        until_start_time_seconds = (
+            self.adr_start - now_utc).total_seconds()
+        until_end_time_seconds = (
+            self.adr_start - self.adr_event_ends).total_seconds()
+        
+        await self.event_do(until_start_time_seconds,
+                            'go')
+        await self.event_do(until_start_time_seconds+until_end_time_seconds,
+                            'stop')
+        
+        
     async def handle_event(self, event):
         """
         Do something based on the event.
@@ -181,76 +177,11 @@ class MyVen():
         loop.create_task(self.event_checkr())
         return "optIn"
 
-    async def event_status(self):
-        now_utc = datetime.now(timezone.utc)
-
-        until_start_time_seconds = (
-            self.adr_start - now_utc).total_seconds()
-
-        until_end_time_seconds = (
-            self.adr_event_ends - now_utc).total_seconds()
-
-        if until_start_time_seconds > 0:
-            if abs(now_utc - self.last_scan).total_seconds() > 15:
-                print("TIME UNTIL EVENT START IN SECONDS: ",
-                      round(until_start_time_seconds))
-                print("TIME UNTIL EVENT START IN MINUTES: ",
-                      until_start_time_seconds//60)
-                print("TIME UNTIL EVENT START IN HOURS: ",
-                      until_start_time_seconds//60//60)
-                self.last_scan = now_utc
-            await asyncio.sleep(1)
-
-        # check if the demand response event is active or not
-        elif now_utc >= ven_client.adr_start and now_utc < ven_client.adr_event_ends:
-            self.adr_event_go = True
-            if abs(now_utc - self.last_scan).total_seconds() > 15:
-                print("TIME UNTIL EVENT END IN SECONDS: ",
-                      round(until_end_time_seconds))
-                print("TIME UNTIL EVENT END IN MINUTES: ",
-                      until_end_time_seconds//60)
-                print("TIME UNTIL EVENT END IN HOURS: ",
-                      until_end_time_seconds//60//60)
-                self.last_scan = now_utc
-            await asyncio.sleep(1)
-
-        elif (
-            now_utc > ven_client.adr_event_ends
-            and self.adr_event_go == True
-            and self.adr_event_stop == False
-            and self.bacnet_sig_change_success == True
-            and self.bacnet_sig_revert_success == False
-        ):
-            self.adr_event_stop = True
-
-        else:
-            pass
-
-
-    async def start_event(self):
-        if (
-            self.adr_event_go == True
-            and self.adr_event_stop == False
-            and self.bacnet_sig_change_success == False
-            and self.bacnet_sig_revert_success == False
-        ):
-            self.bacnet_payload_value = LOAD_SHED_GO_VAL
-
-    async def stop_event(self):
-        if (
-            self.adr_event_go == True
-            and self.adr_event_stop == True
-            and self.bacnet_sig_change_success == True
-            and self.bacnet_sig_revert_success == False
-        ):
-            self.bacnet_payload_value = NORMAL_OPERATIONS
-
-
 
     def make_ven_client(self):
         client = OpenADRClient(ven_name=VEN_NAME,
                                vtn_url=VTN_URL)
-        client.add_report(callback=self.collect_report_value,
+        client.add_report(callback=self.modbus_meter_reader,
                           resource_id='main_meter',
                           measurement='power',
                           sampling_rate=timedelta(seconds=10))
@@ -269,9 +200,6 @@ if __name__ == "__main__":
                       default=5000,
                       help="Port number to run web app")
 
-    args.add_argument("--use-modbus", default=False, action="store_true")
-    args.add_argument("--no-modbus", dest="use-modbus",
-                      action="store_false")
     
     args.add_argument("--use-bacnet", default=False, action="store_true")
     args.add_argument("--no-bacnet", dest="use-bacnet",
@@ -284,18 +212,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print("use_bacnet: ",args.use_bacnet)
     print("use_openadr: ",args.use_openadr)
-    print("use_modbus: ",args.use_modbus)
 
     ven_client = MyVen()
 
-    if args.use_openadr or args.use_modbus:
+    if args.use_openadr:
         loop = asyncio.get_event_loop()
         
-        if args.use_openadr:
-            loop.create_task(ven_client.make_ven_client().run())
-        if args.use_modbus:
-            loop.create_task(ven_client.modbus_meter_reader())
-
+        loop.create_task(ven_client.make_ven_client().run())
         t1 = threading.Thread(
             target=lambda: loop.run_forever())
         t1.setDaemon(True)
